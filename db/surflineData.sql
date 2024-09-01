85317636199bdff690b5bce301d786599d5074d7
Kyle Martinelli <kylelmartinelli@gmail.com>
2024-08-31 19:49:25 -0700

    Database changed to postgres becuase it allows for array types. database schema complete and local instance created.

--- db/surflineData.sql
+++ db/surflineData.sql
@@ -1,78 +1,72 @@
-DROP DATABASE IF EXISTS surflineData;
-CREATE DATABASE surflineData;
-
-USE surflineData;
 
 DROP TABLE IF EXISTS spots;
 
 CREATE TABLE IF NOT EXISTS spots(
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `surflineId` INT UNSIGNED NOT NULL,
-  `surflineUrl` VARCHAR(255) NOT NULL,
-  `spotName` VARCHAR(255) NOT NULL,
-  `lat` DECIMAL(8,6),
-  `lon` DECIMAL(9,6),
-  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY(id),
-  UNIQUE KEY (surflineId)
+  id  SERIAL PRIMARY KEY,
+  surflineId INT NOT NULL UNIQUE,
+  surflineUrl VARCHAR(255) NOT NULL,
+  spotName VARCHAR(255) NOT NULL,
+  lat DECIMAL(8,6),
+  lon DECIMAL(9,6),
+  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS idealConditions;
 
 CREATE TABLE IF NOT EXISTS idealConditions(
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `spotId` INT UNSIGNED NOT NULL,
-  `idealSeason` VARCHAR(255) NOT NULL,
-  `idealSeasonDescription` VARCHAR(255) NOT NULL,
-  `idealTide` VARCHAR(255) NOT NULL,
-  `idealTideDescription` VARCHAR(255) NOT NULL,
-  `idealSize` VARCHAR(255) NOT NULL,
-  `idealSizeDescription` VARCHAR(255) NOT NULL,
-  `idealWindDirections` VARCHAR(255) NOT NULL,
-  `idealWindDirectionsDescription` VARCHAR(255) NOT NULL,
-  `idealSwellDirections` VARCHAR(255) NOT NULL,
-  `idealSwellDirectionsDescription` VARCHAR(255) NOT NULL,
-  `abilityLevels` VARCHAR(255) NOT NULL,
-  `abilityDescription` VARCHAR(255) NOT NULL,
-  `bottomType` VARCHAR(255) NOT NULL,
-  `bottomTypeDescription` VARCHAR(255) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  spotId INT NOT NULL,
+  idealSeason VARCHAR(255)[],
+  idealSeasonDescription VARCHAR(255) NOT NULL,
+  idealTide VARCHAR(255)[],
+  idealTideDescription VARCHAR(255) NOT NULL,
+  idealSize VARCHAR(255)[],
+  idealSizeDescription VARCHAR(255) NOT NULL,
+  idealWindDirections VARCHAR(255)[],
+  idealWindDirectionsDescription VARCHAR(255) NOT NULL,
+  idealSwellDirections VARCHAR(255)[],
+  idealSwellDirectionsDescription VARCHAR(255) NOT NULL,
 
-  PRIMARY KEY(id),
+
   FOREIGN KEY(spotId) REFERENCES spots(id) ON DELETE CASCADE
 );
 
 
 DROP TABLE IF EXISTS spotCharacteristics;
 CREATE TABLE IF NOT EXISTS spotCharacteristics(
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `spotId` INT UNSIGNED NOT NULL,
-  `crowdFactorDescription` VARCHAR(255) NOT NULL,
-  `crowdFactorRating` INT UNSIGNED NOT NULL,
-  `crowdFactorSummary` VARCHAR(255) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  spotId INT NOT NULL,
+  crowdFactorDescription VARCHAR(255) NOT NULL,
+  crowdFactorRating INT NOT NULL,
+  crowdFactorSummary VARCHAR(255) NOT NULL,
 
-  `localVibeDescription` VARCHAR(255) NOT NULL,
-  `localVibeRating` INT UNSIGNED NOT NULL,
-  `localVibeSummary` VARCHAR(255) NOT NULL,
+  localVibeDescription VARCHAR(255) NOT NULL,
+  localVibeRating INT NOT NULL,
+  localVibeSummary VARCHAR(255) NOT NULL,
 
-  `shoulderBurnDescription` VARCHAR(255) NOT NULL,
-  `shoulderBurnRating` INT UNSIGNED NOT NULL,
-  `shoulderBurnSummary` VARCHAR(255) NOT NULL,
+  shoulderBurnDescription VARCHAR(255) NOT NULL,
+  shoulderBurnRating INT NOT NULL,
+  shoulderBurnSummary VARCHAR(255) NOT NULL,
 
-  `spotRatingDescription` VARCHAR(255) NOT NULL,
-  `spotRatingRating` INT UNSIGNED NOT NULL,
-  `spotRatingSummary` VARCHAR(255) NOT NULL,
+  spotRatingDescription VARCHAR(255) NOT NULL,
+  spotRatingRating INT NOT NULL,
+  spotRatingSummary VARCHAR(255) NOT NULL,
 
-  `waterQualityDescription` VARCHAR(255) NOT NULL,
-  `waterQualityRating` INT UNSIGNED NOT NULL,
-  `waterQualitySummary` VARCHAR(255) NOT NULL,
+  waterQualityDescription VARCHAR(255) NOT NULL,
+  waterQualityRating INT NOT NULL,
+  waterQualitySummary VARCHAR(255) NOT NULL,
 
-  `boardTypes`  VARCHAR(255) NOT NULL,
+  abilityLevels VARCHAR(255)[],
+  abilityDescription VARCHAR(255) NOT NULL,
 
-  `breakType`  VARCHAR(255) NOT NULL,
-  `spotDescription` TEXT,
+  bottomType VARCHAR(255)[],
+  bottomTypeDescription VARCHAR(255) NOT NULL,
 
+  boardTypes  VARCHAR(255)[],
 
-  PRIMARY KEY(id),
+  breakType  VARCHAR(255)[],
+  spotDescription TEXT,
+
   FOREIGN KEY (spotId) REFERENCES spots(id)
 
 );
@@ -81,20 +75,21 @@ CREATE TABLE IF NOT EXISTS spotCharacteristics(
 DROP TABLE IF EXISTS spotBreadCrumbs;
 
 CREATE TABLE IF NOT EXISTS spotBreadCrumbs(
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `spotId` INT UNSIGNED NOT NULL,
-  `country` VARCHAR(255) NOT NULL,
-  `countryId` INT UNSIGNED NOT NULL,
-  `region` VARCHAR(255) NOT NULL,
-  `regionId` INT UNSIGNED NOT NULL,
-  `subRegion` VARCHAR(255) NOT NULL,
-  `subRegionId` INT UNSIGNED NOT NULL,
-  `area` VARCHAR(255) DEFAULT NULL,
-  `areaId` INT UNSIGNED DEFAULT NULL,
+  id SERIAL PRIMARY KEY,
+  spotId INT NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  countryId INT NOT NULL,
+  region VARCHAR(255) NOT NULL,
+  regionId INT NOT NULL,
+  subRegion VARCHAR(255) NOT NULL,
+  subRegionId INT NOT NULL,
+  area VARCHAR(255) DEFAULT NULL,
+  areaId INT DEFAULT NULL,
 
-  PRIMARY KEY (id),
+
   FOREIGN KEY(spotId) REFERENCES spots(id)
 );
 
 
 -- mysql -u root -h localhost -p < db/surflineData.sql
+-- psql surflinedata -a -f db/surflineData.sql
